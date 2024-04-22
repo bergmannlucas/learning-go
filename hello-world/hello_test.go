c5c6b3aa4b74a8758be14b2cfbe85d244253caa1
bergmannlucas <lucaasberg@gmail.com>
2024-04-22 14:32:02 -0300

    feat: add improved version of hello-world pt2

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,16 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Lucas")
+		got := Hello("Lucas", "")
 		want := "Hello, Lucas"
-
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
